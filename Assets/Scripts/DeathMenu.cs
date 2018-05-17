82fa11b388243d532406f5b3f1963eed97638242
DESKTOP-0BTH82I\Gebruiker <mjakobs11@hotmail.nl>
2018-05-17 08:39:48 +0200

    suh
    dude

---
+++ Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour 
+{
+    public string mainMenuLevel;
+    public GameManager gameManager;
+
+    public void RestartGame()
+    {
+        gameManager.Reset();
+    }
+
+    public void QuitToMain()
+    {
+        SceneManager.LoadScene(mainMenuLevel);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# endless-runner-tutorial
+Tutorial followed from [YouTube by gamesplusjames](https://www.youtube.com/playlist?list=PLiyfvmtjWC_XmdYfXm2i1AQ3lKrEPgc9-)
